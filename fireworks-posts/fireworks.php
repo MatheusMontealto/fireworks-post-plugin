0028879ee570f2074a518c03852151fc1edf0bbe
Matheus dos Santos Montealto <70967012+MatheusMontealto@users.noreply.github.com>
2020-09-15 10:27:27 -0300

    Add files via upload

--- fireworks-posts/fireworks.php
+++ fireworks-posts/fireworks.php
@@ -3,20 +3,19 @@
 /*
     Plugin name: Fireworks Posts
     Plugin uri: https://github.com/MatheusMontealto
-    Description: Plugin que gera fogos de artifício ao fundo de um post.
-    Version: 1.0  
+    Description: Plugin que gera fogos de artifício ao fundo do site.
+    Version: 0.2  
     Author: Matheus Montealto
     Author uri: https://github.com/MatheusMontealto
     License: GPLv2 or later
 */
 
 
-function firework(){ ?>
+function firework(){ 
     
-<body background="https://cdn.edu.buncee.com/assets/b5c776c296840e5dda8cf0fdfc47ce6d/animation-holiday-fireworks-122618-7.gif?timestamp=1545838203">
-</body>
-<?php
+    echo "<style> body {background-image: url('https://cdn.edu.buncee.com/assets/b5c776c296840e5dda8cf0fdfc47ce6d/animation-holiday-fireworks-122618-7.gif?timestamp=1545838203') !important;}}</style>";
+    
 }
-add_filter('the_content','firework'); // Chama a função no the content do post
+add_filter('init','firework'); // Chama a função no init
 
 ?>
